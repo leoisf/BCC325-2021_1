f685eda918ff6e6429d7041374848b7f02fee9a3
Leonardo <leoisf@gmail.com>
2021-01-22 09:03:03 -0300

    adição da classe ambiente + comentários

--- entities.py
+++ entities.py
@@ -1,15 +1,39 @@
-"""Esta classe implementa uma interface para um ambiente"""
+
 class Agent:
+    """Esta classe implementa uma interface para um agente"""
+
     def __init__(self, env):
+        """ Construdor da classe Agent
+            Args:
+            env: referência para um ambinente
+        """        
         self.env = env
 
     def act(self):
+        """ 
+            defien a ação do agente
+            gera o erro NotImplementedError caso o método não for implementado ou sobrescrito
+            
+        """
         raise NotImplementedError('act')
 
 
+
 class Environment:
+    """Esta classe implementa uma interface para um ambiente"""
+    
     def initial_percepts(self):
+        """
+            retorna a percepção inicial do ambiente
+            gera o erro NotImplementedError caso o método não for implementado ou sobrescrito
+
+        """
        raise NotImplementedError('initial_percepts')
 
     def signal(self, action):
+         """
+            retorna a percepção do ambiente após a execução da ação
+            gera o erro NotImplementedError caso o método não for implementado ou sobrescrito
+
+        """
         raise  NotImplementedError('signal')
